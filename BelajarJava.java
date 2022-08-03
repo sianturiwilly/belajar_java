ae9a97e75653e6ee04a7914b8b6c203ca9a74702
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-03 11:59:59 +0700

    Commit keempat

--- BelajarJava.java
+++ BelajarJava.java
@@ -1,22 +1,25 @@
+import java.util.Scanner;
+   
 class BelajarJava {
   public static void main(String args[]){
-        
-    int     var1 = 56;
-    double  var2 = 0.543;
-    boolean var3 = true;
-    char    var4 = 'A';
-    String  var5 = "Duniailkom";
-    int[]   var6 = {4, 7, 3, 8, 1};
+       
+    Scanner input = new Scanner(System.in);
+       
+    String[] siswa = new String[3];
+   
+    System.out.print("Siswa pertama: ");
+    siswa[0] = input.nextLine();
+      
+    System.out.print("Siswa kedua: ");
+    siswa[1] = input.nextLine();
      
-    System.out.println(var1);
-    System.out.println(var2);
-    System.out.println(var3);
-    System.out.println(var4);
-    System.out.println(var5);
-     
-    for (int i : var6) {
-      System.out.print(i +" ");
-    }
- 
+    System.out.print("Siswa ketiga: ");
+    siswa[2] = input.nextLine();
+       
+    System.out.println();
+    System.out.println("## Hasil ##");
+       
+    System.out.print("Nama siswa adalah: ");
+    System.out.println(siswa[0]+", "+siswa[1]+", "+siswa[2]);
   }
 }
