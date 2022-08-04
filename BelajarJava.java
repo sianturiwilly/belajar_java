4c24cb73b17f3194dfcf250209998b63ac8c8b98
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-04 14:18:13 +0700

    second commit

--- BelajarJava.java
+++ BelajarJava.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
-   
 class BelajarJava {
   public static void main(String args[]){
-       
-    Scanner input = new Scanner(System.in);
-       
-    String[] siswa = new String[3];
-   
-    System.out.print("Siswa pertama: ");
-    siswa[0] = input.nextLine();
-      
-    System.out.print("Siswa kedua: ");
-    siswa[1] = input.nextLine();
+        
+    String a = "Duniailkom";
+    String hasil;
      
-    System.out.print("Siswa ketiga: ");
-    siswa[2] = input.nextLine();
-       
-    System.out.println();
-    System.out.println("## Hasil ##");
-       
-    System.out.print("Nama siswa adalah: ");
-    System.out.println(siswa[0]+", "+siswa[1]+", "+siswa[2]);
+    hasil = a == "Duniailkom" ? "Mari belajar Java" : "Ops...";
+    System.out.println(hasil);  
+     
+    hasil = a == "Google" ? "Googling..." : "Bukan web google";
+    System.out.println(hasil); 
+     
   }
 }
