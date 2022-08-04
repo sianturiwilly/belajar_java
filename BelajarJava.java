cd40c0a7f10c647a800143ad317b1eafd34d9573
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-04 14:50:20 +0700

    Commit ketiga

--- BelajarJava.java
+++ BelajarJava.java
@@ -1,14 +1,29 @@
 class BelajarJava {
   public static void main(String args[]){
-        
-    String a = "Duniailkom";
-    String hasil;
+         
+    int a = 10, b = 10, c = 10, d = 10, e = 10, f = 10; 
      
-    hasil = a == "Duniailkom" ? "Mari belajar Java" : "Ops...";
-    System.out.println(hasil);  
+    System.out.println("Operator assignment gabungan bahasa Java");  
+    System.out.println("========================================"); 
+    System.out.println("Variabel a, b, c, d, e, f = 10 \n"); 
+ 
+    a += 5;
+    System.out.println("Hasil operasi a += 5: "+ a);
      
-    hasil = a == "Google" ? "Googling..." : "Bukan web google";
-    System.out.println(hasil); 
+    b -= 3;
+    System.out.println("Hasil operasi b -= 3: "+ b);
      
+    c *= 3;
+    System.out.println("Hasil operasi c *= 3: "+ c);
+     
+    d /= 3;
+    System.out.println("Hasil operasi d /= 3: "+ d);
+     
+    e %= 3;
+    System.out.println("Hasil operasi e %= 3: "+ e);
+     
+    f <<= 2;
+    System.out.println("Hasil operasi f <<= 2: "+ f);
+       
   }
 }
